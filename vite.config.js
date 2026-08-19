4cb1e95f3ea585cce60d7cee6b79beb519cc56e5
Mcesm <marce.mor.olmedo2@gmail.com>
2026-08-19 18:52:29 +0200

    fix: establecer ruta relativa base en vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configuración recomendada para GitHub Pages (base relativa para assets)
 export default defineConfig({
   plugins: [react()],
+  base: './',
 })
